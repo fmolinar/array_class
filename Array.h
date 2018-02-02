12eb4ef5e3d21db4cdc6b74b0c0b6db2c22bb99c
Fernando Molina Rodriguez <fer.molina.rdz@gmail.com>
2018-02-01 16:22:24 -0800

    Add files via upload

--- Array.h
+++ Array.h
@@ -1,6 +1,10 @@
 #ifndef ARRAY_H_INCLUDED
 #define ARRAY_H_INCLUDED
 
+#include <tuple>
+
+using namespace std;
+
 class Array
 {
 public:
@@ -8,7 +12,13 @@ public:
     Array(int); // copy constructor
     ~Array(); // destructor
 
+    // Accessor + Modifiers
     int getsize(); // Function to access the size
+    int getElement(int); // Function to get the element @ the specify index
+    void setElement(int,int);// Function to set an element for a given index
+
+    int& operator[] (unsigned);// Overloading [] operator to access elements in array style to check for index out of bound.
+    bool isThere(int);// Function to check if an element is in the list
     void Display(); // Display the content of the array
     void addElementBeginnig(int); // Function to Add an element at the beginning
     void addElementEnd(int); // Function to Add an element at the end
@@ -20,14 +30,14 @@ public:
     int* oddNumber(); // return array with odd numbers
     int* operator+(const Array&)const; // concat 2 arrays of size n and m into a new array
 
-    int sizeOfArray; // size of the array
-
-    int *arrayData; // array
+//    int sizeOfArray; // size of the array
+//    int *arrayData; // array
 
 protected:
 
 private:
-
+    int sizeOfArray; // size of the array
+    int *arrayData; // array
 };
 
 
